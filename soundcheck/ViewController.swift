f56083761dc5e20bc37b3e4656406350badcaf59
champ <dev@skillane.com>
2016-09-13 21:44:50 +0700

    create test app

---
+++ soundcheck/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  soundcheck
+//
+//  Created by Thanawith Munkatitum on 9/13/2559 BE.
+//  Copyright © 2559 Kittipol Munkatitum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ soundcheckTests/soundcheckTests.swift
@@ -0,0 +1,36 @@
+//
+//  soundcheckTests.swift
+//  soundcheckTests
+//
+//  Created by Thanawith Munkatitum on 9/13/2559 BE.
+//  Copyright © 2559 Kittipol Munkatitum. All rights reserved.
+//
+
+import XCTest
+@testable import soundcheck
+
+class soundcheckTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
